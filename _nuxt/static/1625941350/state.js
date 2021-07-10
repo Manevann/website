ab12acc97fd0b3c2ea5c0ce5d6b669a69594b7ad
TobbenTM <TobbenTM@users.noreply.github.com>
2021-07-10 18:22:31 +0000

    deploy: 3fec19da7444763c82527cb14a59d6c5877b7b19

---
+++ _nuxt/static/1625941350/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625941350",layout:"headerless",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c32ccc1c"}}}}(null,"\u002F"));
