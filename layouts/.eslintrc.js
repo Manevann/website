86b144ae01a04d743f72d704f240a9e9b5472666
Tobias Lønnerød Madsen <m@dsen.tv>
2021-12-19 22:56:49 +0100

    Fix broken ESLint after upgrade

---
+++ layouts/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'vue/multi-word-component-names': 0 // Layouts can be single word
+  }
+}
---
+++ pages/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'vue/multi-word-component-names': 0 // Pages can be single words
+  }
+}
